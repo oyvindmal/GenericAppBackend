7d0e11886b3700277bdd9832ea648291babbe943
Theodor Tonum <theodor@tonum.no>
2012-02-05 17:07:00 +0100

    Underscore style for attribute names

--- spec/models/generic_attribute_spec.rb
+++ spec/models/generic_attribute_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe GenericAttribute do
-  describe "validations" do
-    let(:generic_attribute) { FactoryGirl.create(:generic_attribute) }
+  let(:generic_attribute) { FactoryGirl.create(:generic_attribute) }
 
+  describe "validations" do
     it "should come valid from the factory" do
       generic_attribute.should be_valid
     end
@@ -16,6 +16,19 @@ describe GenericAttribute do
     it "should be invalid without a model" do
       generic_attribute.generic_model = nil
       generic_attribute.should be_invalid
+    end
+  end
+
+  describe "naming conventions" do
+    it "should underscore attribute name" do
+      generic_attribute.name = "MyAttribute"
+      generic_attribute.name.should == "my_attribute"
+
+      generic_attribute.name = "my attribute"
+      generic_attribute.name.should == "my_attribute"
+
+      generic_attribute.name = "my-attribute"
+      generic_attribute.name.should == "my_attribute"
     end
   end
 end
